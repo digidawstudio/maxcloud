1a38d5e0559b1445232bf9268453aa3271c924c8
reingalrt <adorufus@gmail.com>
2023-09-17 18:48:55 +0700

    push

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -8,6 +8,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    /**TODO: Change navigation to auth first or if you have alredy created auth function please do check if users have session then push the user into home screen else push the user into auth screen
+     *
+     */
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Container()));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Image(image: AssetImage("assets/images/splashscreen.jpg"), fit: BoxFit.cover,);
