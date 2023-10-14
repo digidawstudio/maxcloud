cf7c6820ef3e9809558ead97530a0f8c50ae08a6
reingalrt <adorufus@gmail.com>
2023-10-14 22:22:51 +0700

    finishing otp validation

--- lib/repository/auth/otp.model.dart
+++ lib/repository/auth/otp.model.dart
@@ -6,11 +6,11 @@ class OtpModel {
 
   OtpModel.fromJson(Map<String, dynamic> json) {
     message = json['message'];
-    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+    final Map<String, dynamic> data = Map<String, dynamic>();
     data['message'] = this.message;
     if (this.data != null) {
       data['data'] = this.data!.toJson();
@@ -31,9 +31,50 @@ class Data {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+    final Map<String, dynamic> data = Map<String, dynamic>();
     data['credential'] = this.credential;
     data['request_type'] = this.requestType;
     return data;
   }
 }
+
+class OtpValidate {
+  String? message;
+  OtpValidateData? data;
+
+  OtpValidate({this.message, this.data});
+
+  OtpValidate.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    data = json['data'] != null ? OtpValidateData.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class OtpValidateData {
+  String? accessToken;
+  String? customer;
+
+  OtpValidateData({this.accessToken, this.customer});
+
+  OtpValidateData.fromJson(Map<String, dynamic> json) {
+    accessToken = json['access_token'];
+    customer = json['customer'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['access_token'] = this.accessToken;
+    data['customer'] = this.customer;
+    return data;
+  }
+}
+
